6aa985868ec9ee235bc43a540488b6c56419cd66
Oluwajuwon-F <fawoleoluwajuwon@gmail.com>
2019-10-09 19:14:35 +0100

    Edit Profile Page

---
+++ Bedtimestory/app/src/main/java/com/dragonlegend/kidstories/EditProfile.java
@@ -0,0 +1,29 @@
+package com.dragonlegend.kidstories;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class EditProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+
+        getSupportActionBar().setTitle("Select Image");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+
